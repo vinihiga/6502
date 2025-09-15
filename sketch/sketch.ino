b0bc30c94f7cbcbfdbe925266cd41b2928bd00cb
Vinicius Higa <viniciushigadev@gmail.com>
2025-09-15 19:29:39 -0300

    fix: unecessary charge pin

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,7 +1,8 @@
-const int chargePin = A0;
+// const int chargePin = 53;
 const int clockPin = A1;
 const int resetPin = A2;
 const int rwPin = A3;
+const int debugPin = 51;
 
 const int dataPins[] = {52, 50, 48, 46, 44, 42, 40, 38};
 const int addressPins[] = {36, 34, 32, 30, 28, 26, 24, 22, A7, A8, A9, A10, A11, A12, A13, A14, A15};
@@ -10,37 +11,65 @@ int clockCycle = 0;
 const int MEM_SIZE = 4096;
 byte memory[MEM_SIZE];
 
+unsigned long addressValue = 0;
+
 void setup() {
   Serial.begin(9600);
   initializeMemory();
   
-  pinMode(chargePin, OUTPUT);
+  // pinMode(chargePin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(resetPin, INPUT);
   pinMode(rwPin, INPUT);
+  pinMode(debugPin, INPUT);
+
+  for (int i = 0; i < 16; i++) {
+    pinMode(addressPins[i], INPUT);
+  }
 
   Serial.println("Iniciando CPU");
-  digitalWrite(chargePin, HIGH); // Liga o IC
+  digitalWrite(resetPin, LOW);
+  // digitalWrite(chargePin, HIGH); // Liga o IC
 }
 
 void loop() {
-  delay(500);
   digitalWrite(clockPin, HIGH);
+  delay(500);
+  readAddress();
+
+  digitalWrite(clockPin, LOW);
+  delay(500);
   onClock();
 
-  delay(500);
-  digitalWrite(clockPin, LOW);
-  onClock();
+  clockCycle += 1;
+
+  if (clockCycle == 2) {
+    digitalWrite(resetPin, HIGH);
+    Serial.println("Tempo inicial de 2 clocks ultrapassado. Iniciando leitura de bus address e data.");
+  }
+
+  // Using debug pin to check if pull up is working.
+  int isDebugPowered = digitalRead(debugPin) == HIGH;
+  Serial.print("Debug Pin: ");
+  Serial.println(isDebugPowered);
+}
+
+void readAddress() {
+  if (clockCycle < 2) {
+    return;
+  }
+
+  addressValue = 0;
+
+  for (int i = 0; i < 16; i++) {
+    if (digitalRead(addressPins[i]) == HIGH) {
+      addressValue |= (1UL << i);
+    }
+  }
 }
 
 void onClock() {
   if (clockCycle < 2) {
-    clockCycle += 1;
-    return;
-  } else if (clockCycle == 2) {
-    Serial.println("Tempo inicial de 2 clocks ultrapassado. Iniciando leitura de bus address e data.");
-    digitalWrite(resetPin, HIGH); // Permite o push button "triggar" o RESET
-    clockCycle += 1;
     return;
   }
 
@@ -56,16 +85,7 @@ void onClock() {
 
 void handleRw() {
   byte dataValue = 0;
-  unsigned long addressValue = 0;
   int rwStatus = digitalRead(rwPin);
-  
-  // Configura os pinos de endereço como ENTRADA para ler o endereço
-  for (int i = 0; i < 16; i++) {
-    pinMode(addressPins[i], INPUT);
-    if (digitalRead(addressPins[i]) == HIGH) {
-      addressValue |= (1UL << i);
-    }
-  }
 
   // Se for uma operação de LEITURA (rwPin == HIGH)
   if (rwStatus == HIGH) {
