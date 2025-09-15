c879bda8fb7083e167254f255fe4f732dbfcebd7
Vinicius Higa <viniciushigadev@gmail.com>
2025-09-15 00:08:50 -0300

    fix: boot

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,18 +1,20 @@
 const int chargePin = A0;
-const int clockPin = 2;
-const int resetPin = A1;
-// const int debugPin = 53;
+const int clockPin = A1;
+const int resetPin = A2;
+const int debugPin = 53;
 
-const int addressPins[] = {52, 50, 48, 46, 44, 42, 40, 38};
-const int dataPins[] = {36, 34, 32, 30, 28, 26, 24, 22};
+const int dataPins[] = {52, 50, 48, 46, 44, 42, 40, 38};
+const int addressPins[] = {36, 34, 32, 30, 28, 26, 24, 22};
+
+int clockCycle = 0;
 
 void setup() {
   Serial.begin(9600);
   
   pinMode(chargePin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  pinMode(resetPin, OUTPUT);
-  // pinMode(debugPin, INPUT);
+  pinMode(resetPin, INPUT);
+  pinMode(debugPin, INPUT);
 
   for (int i = 0; i < 8; i++) {
     pinMode(addressPins[i], INPUT);
@@ -22,32 +24,37 @@ void setup() {
     pinMode(dataPins[i], INPUT);
   }
 
+  Serial.println("Iniciando CPU");
   digitalWrite(chargePin, HIGH); // Liga o IC
-  digitalWrite(resetPin, LOW);   // Ativa o reset
-  delay(100);                   // Espera um momento para o reset ser reconhecido
-  digitalWrite(resetPin, HIGH);  // Desativa o reset, liberando o processador
 }
 
 void loop() {
+  delay(100);
   digitalWrite(clockPin, HIGH);
-  handleClock();
-  delay(500);
 
+  delay(100);
   digitalWrite(clockPin, LOW);
-  delay(500);
-  // int isDebugPressed = (digitalRead(debugPin) == HIGH);
-  // Serial.println(isDebugPressed);
+
+  clockCycle += 1;
+
+  if (clockCycle == 2) {
+    Serial.println("Tempo inicial de 2 clocks ultrapassado. Iniciando leitura de bus address e data.");
+    digitalWrite(resetPin, HIGH); // Permite o push button "triggar" o RESET
+  } else if (clockCycle > 2) {
+    handleClock();
+  }
 }
 
 void handleClock() {
   byte dataValue = 0;
+  byte addressValue = 0;
+
   for (int i = 0; i < 8; i++) {
     if (digitalRead(dataPins[i]) == HIGH) {
       dataValue |= (1 << i);
     }
   }
 
-  byte addressValue = 0;
   for (int i = 0; i < 8; i++) {
     if (digitalRead(addressPins[i]) == HIGH) {
       addressValue |= (1 << i);
@@ -58,4 +65,8 @@ void handleClock() {
   Serial.print(addressValue, HEX);
   Serial.print(" | Data: ");
   Serial.println(dataValue, HEX);
+
+  int isDebugPressed = (digitalRead(debugPin) == HIGH);
+  Serial.print("isResetPressed: ");
+  Serial.println(isDebugPressed);
 }
